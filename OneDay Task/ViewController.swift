4ade546fa5a24cbe659a0a768f633496fb9dbb5c
Resan-ios-developer <coollong990725i@yahoo.com>
2020-07-15 17:32:45 +0900

    Initial Commit

---
+++ OneDay Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneDay Task
+//
+//  Created by 永井涼 on 2020/07/15.
+//  Copyright © 2020 永井涼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
